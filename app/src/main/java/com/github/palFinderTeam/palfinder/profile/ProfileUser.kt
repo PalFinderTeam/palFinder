5dccee85eedca7d51f383df050ec1f71fd937e53
TruddyTheDuddi <ddemkoo.nolkooo@gmail.com>
2022-03-04 21:53:43 +0100

    Create user class base and activity for profile

---
+++ app/src/main/java/com/github/palFinderTeam/palfinder/profile/ProfileUser.kt
@@ -0,0 +1,13 @@
+package com.github.palFinderTeam.palfinder.profile
+
+import java.util.*
+
+/**
+ * A class to hold the data for a user to be displayed on the profile activity
+ */
+class ProfileUser(name: String, surname: String, username: String, joinDate: Date) {
+
+}
+
+// Create user class, with Name, username, date of join. Can request a friendly formatted text saying
+// 'joined 5 days ago'.
