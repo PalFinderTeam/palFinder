be8e9389dd85418650c85a35fe13398dff44c689
TruddyTheDuddi <ddemkoo.nolkooo@gmail.com>
2022-03-15 22:13:14 +0100

    Recode picture loading

--- app/src/main/java/com/github/palFinderTeam/palfinder/utils/image/ImageInstance.kt
+++ app/src/main/java/com/github/palFinderTeam/palfinder/utils/image/ImageInstance.kt
@@ -1,63 +1,85 @@
 package com.github.palFinderTeam.palfinder.utils.image
 
-import android.graphics.BitmapFactory
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.view.Window
+import android.graphics.Bitmap
+import android.util.Log
 import android.widget.ImageView
-import android.widget.TextView
+import androidx.lifecycle.ViewModel
 import com.github.palFinderTeam.palfinder.R
-import com.google.firebase.storage.FirebaseStorage
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import java.io.File
 import java.io.Serializable
+import java.lang.Exception
 
-
+/**
+ * Download an image from a given URL and injects it into an
+ * `ImageView`. This can be a Firebase storage image or a raw
+ * HTTP(S) link
+ * @param imgURL String image URL path
+ */
 data class ImageInstance(
-    val imgFirebasePath : String
-) : Serializable{
+    val imgURL : String
+) : Serializable, ViewModel(){
+
+    private val TAG = "ImageInstance";
 
     private lateinit var localFile : File
-    var isInit : Boolean = false
+    private var bitmapCache : Bitmap? = null
+    var isCached : Boolean = false
 
     /**
      * Loads the image asynchronously into the desired Image View
      * @param view ImageView that needs to be change
      */
     fun loadImageInto(view : ImageView){
-        println("Loading image")
-        if (isInit) {
-            println("Image is already existing!")
-            val bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
-            view.setImageBitmap(bitmap)
+        // Is image cached?
+        if (isCached) {
+            Log.d(TAG, "Image is already existing!")
+            view.setImageBitmap(bitmapCache)
         } else {
-            println("Fetching from Database")
-            runBlocking {
-                launch { fetchFromDB(view) }
+            Log.d(TAG, "Fetching from Database")
+
+            GlobalScope.launch(Dispatchers.Main){
+                try {
+                    view.setImageResource(android.R.color.background_dark)
+                    view.alpha = 0.3f
+
+                    withContext(Dispatchers.IO){
+                        fetchFromDB()
+                    }
+
+                    view.setImageBitmap(bitmapCache)
+                } catch (e: Exception) {
+                    view.setImageResource(R.drawable.not_found)
+                    isCached = false
+                } finally {
+                    view.alpha = 1.0f
+                }
             }
         }
     }
 
-    private fun fetchFromDB(view : ImageView){
-        // Create loading effect
-        view.setImageResource(android.R.color.background_dark)
-        view.alpha = 0.3f
-
-        // References for the image
-        val storageRef = FirebaseStorage.getInstance().reference.child(imgFirebasePath)
-        localFile = File.createTempFile("tmpImg", "png")
-
-        // Try download image
-        storageRef.getFile(localFile).addOnSuccessListener {
-            val bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
-            view.setImageBitmap(bitmap)
-            isInit = true
-        }.addOnFailureListener{
-            println("Failed to load image")
-            view.setImageResource(R.drawable.not_found)
+    /**
+     * Clears cached image, forces to re-download image when
+     * loading image into another view
+     */
+    fun clearImageCache(){
+        if (isCached) {
+            bitmapCache = null
+            isCached = false
         }
-        view.alpha = 1f
+    }
+
+    /**
+     * Load the online image with the correct tools
+     */
+    private suspend fun fetchFromDB(){
+        // Select corresponding fetcher
+        val fetcher : ImageFetcher = if (UrlFormat.getUrlType(imgURL) == UrlFormat.URL_IS_FIREBASE) {
+            ImageFetcherFirebase(imgURL)
+        } else {
+            ImageFetcherHttp(imgURL)
+        }
+        bitmapCache = fetcher.fetchImage()
+        isCached = true
     }
 }
