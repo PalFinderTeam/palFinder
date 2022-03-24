06d5bd92b36d483ae17a5c35f169226e9825b680
TheblueMan003 <31632552+TheblueMan003@users.noreply.github.com>
2022-03-24 09:38:45 +0100

    Change Send Test Nav Bar Test

--- app/src/androidTest/java/com/github/palFinderTeam/palfinder/navbar/NavBarTest.kt
+++ app/src/androidTest/java/com/github/palFinderTeam/palfinder/navbar/NavBarTest.kt
@@ -7,11 +7,14 @@ import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.init
+import androidx.test.espresso.intent.Intents.release
 import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.matcher.ViewMatchers
 import com.github.palFinderTeam.palfinder.MainActivity
 import com.github.palFinderTeam.palfinder.R
+import com.github.palFinderTeam.palfinder.map.MapsActivity
 import com.github.palFinderTeam.palfinder.meetups.activities.MeetUpCreation
+import com.github.palFinderTeam.palfinder.meetups.activities.MeetupListActivity
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Rule
@@ -23,7 +26,7 @@ class NavBarTest {
     val hiltRule = HiltAndroidRule(this)
 
     @Test
-    fun testButton(){
+    fun testCreateButton(){
         val intent = Intent(ApplicationProvider.getApplicationContext(), MainActivity::class.java)
         init()
         ActivityScenario.launch<MainActivity>(intent)
@@ -31,5 +34,30 @@ class NavBarTest {
             ViewMatchers.withId(R.id.nav_bar_create)
         ).perform(ViewActions.click())
         Intents.intended(IntentMatchers.hasComponent(MeetUpCreation::class.java.name))
+        release()
+    }
+
+    @Test
+    fun testMapButton(){
+        val intent = Intent(ApplicationProvider.getApplicationContext(), MainActivity::class.java)
+        init()
+        ActivityScenario.launch<MainActivity>(intent)
+        Espresso.onView(
+            ViewMatchers.withId(R.id.nav_bar_find)
+        ).perform(ViewActions.click())
+        Intents.intended(IntentMatchers.hasComponent(MapsActivity::class.java.name))
+        release()
+    }
+
+    @Test
+    fun testGroupButton(){
+        val intent = Intent(ApplicationProvider.getApplicationContext(), MainActivity::class.java)
+        init()
+        ActivityScenario.launch<MainActivity>(intent)
+        Espresso.onView(
+            ViewMatchers.withId(R.id.nav_bar_groups)
+        ).perform(ViewActions.click())
+        Intents.intended(IntentMatchers.hasComponent(MeetupListActivity::class.java.name))
+        release()
     }
 }
