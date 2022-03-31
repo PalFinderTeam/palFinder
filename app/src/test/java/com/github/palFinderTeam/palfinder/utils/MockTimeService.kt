6f9e86454e7181c294bd08770b92c9829ccf7d22
Christian Fattebert <christian.fattebert@epfl.ch>
2022-03-31 16:53:26 +0200

    Fix Test 2.0

--- app/src/test/java/com/github/palFinderTeam/palfinder/utils/MockTimeService.kt
+++ app/src/test/java/com/github/palFinderTeam/palfinder/utils/MockTimeService.kt
@@ -2,9 +2,13 @@ package com.github.palFinderTeam.palfinder.utils
 
 import android.icu.util.Calendar
 import com.github.palFinderTeam.palfinder.utils.time.TimeService
+import org.mockito.Mockito
 
 class MockTimeService : TimeService {
-    private var date = Calendar.getInstance()
+    private var date = Mockito.mock(Calendar::class.java)
+    init {
+        Mockito.`when`(date.timeInMillis).thenReturn(69)
+    }
     override fun now(): Calendar {
         return date
     }
