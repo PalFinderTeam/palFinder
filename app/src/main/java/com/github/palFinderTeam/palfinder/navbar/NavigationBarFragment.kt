b2b1d93b09b0f016e2c9369bb515673cfbf6627b
Zacharie Tevaearai <zacharie.tevaearai@epfl.ch>
2022-04-06 13:54:05 +0200

    Add navigation component dependencies.

--- app/src/main/java/com/github/palFinderTeam/palfinder/navbar/NavigationBarFragment.kt
+++ app/src/main/java/com/github/palFinderTeam/palfinder/navbar/NavigationBarFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityOptionsCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.github.palFinderTeam.palfinder.R
