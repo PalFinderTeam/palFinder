cad035b38a404d8b09166dd96e85055339418142
Zacharie Tevaearai <zacharie.tevaearai@epfl.ch>
2022-04-14 14:02:07 +0200

    Integrate option menu in main nav.

--- app/src/main/java/com/github/palFinderTeam/palfinder/MainNavActivity.kt
+++ app/src/main/java/com/github/palFinderTeam/palfinder/MainNavActivity.kt
@@ -2,13 +2,23 @@ package com.github.palFinderTeam.palfinder
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
 import com.github.palFinderTeam.palfinder.meetups.activities.MeetUpCreation
+import com.github.palFinderTeam.palfinder.ui.login.LoginActivity
+import com.github.palFinderTeam.palfinder.ui.settings.SettingsActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.tabs.TabLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,9 +26,13 @@ class MainNavActivity : AppCompatActivity() {
 
     private var findState = FindState.MAP
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_nav)
+
+        auth = Firebase.auth
 
         val navController =
             (supportFragmentManager.findFragmentById(R.id.main_content) as NavHostFragment).navController
@@ -109,6 +123,39 @@ class MainNavActivity : AppCompatActivity() {
 
             }
         )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.miLogout -> {
+                //Logout the user
+                auth.signOut()
+                val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken("341371843047-6i3a92lfmcb6555vsj9sb02tnhmkh4c8.apps.googleusercontent.com") //somehow cannot access value through google-service values.xml
+                    .requestEmail()
+                    .build()
+
+                val client = GoogleSignIn.getClient(this, gso)
+                client.signOut()
+                val logoutIntent = Intent(this, LoginActivity::class.java)
+                logoutIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(logoutIntent)
+                return true
+            }
+            R.id.miSettings -> {
+                startActivity(Intent(this, SettingsActivity::class.java))
+                return true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
     }
 
     private fun navItemToPosition(itemId: Int): Int {
