29f168900432ec111a93c774817e038db2a7aca1
atappy <antoine.tappy@epfl.ch>
2022-04-16 12:21:09 +0200

    make criterionGender serial

---
+++ app/src/main/java/com/github/palFinderTeam/palfinder/utils/CriterionGender.kt
@@ -0,0 +1,7 @@
+package com.github.palFinderTeam.palfinder.utils
+
+enum class CriterionGender(val genderName: String) : java.io.Serializable {
+    FEMALE("female"),
+    MALE("male"),
+    ALL("null"),
+}
