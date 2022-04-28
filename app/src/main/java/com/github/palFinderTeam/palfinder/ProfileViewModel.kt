511e42304722354f163f42fd70c008cec0d78bb0
TruddyTheDuddi <ddemkoo.nolkooo@gmail.com>
2022-04-28 17:41:27 +0200

    Fix visibility of variable

--- app/src/main/java/com/github/palFinderTeam/palfinder/ProfileViewModel.kt
+++ app/src/main/java/com/github/palFinderTeam/palfinder/ProfileViewModel.kt
@@ -29,7 +29,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
-    private val profileService: ProfileService,
+    val profileService: ProfileService,
     private val meetUpService: MeetUpRepository
 ) : ViewModel() {
 
