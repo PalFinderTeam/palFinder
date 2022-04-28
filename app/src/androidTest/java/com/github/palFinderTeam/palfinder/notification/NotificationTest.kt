1423ea3b1ae3b3155666fcff974568418ff5d39b
Christian Fattebert <christian.fattebert@epfl.ch>
2022-04-28 16:18:57 +0200

    Tests

--- app/src/androidTest/java/com/github/palFinderTeam/palfinder/notification/NotificationTest.kt
+++ app/src/androidTest/java/com/github/palFinderTeam/palfinder/notification/NotificationTest.kt
@@ -1,6 +1,7 @@
 package com.github.palFinderTeam.palfinder.notification
 
 import android.content.Context
+import android.icu.util.Calendar
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
@@ -10,10 +11,19 @@ import androidx.test.uiautomator.UiObject2
 import androidx.test.uiautomator.Until
 import com.github.palFinderTeam.palfinder.MainActivity
 import com.github.palFinderTeam.palfinder.R
+import com.github.palFinderTeam.palfinder.cache.DictionaryCache
+import com.github.palFinderTeam.palfinder.cache.DictionaryCacheTest
+import com.github.palFinderTeam.palfinder.chat.ChatService
+import com.github.palFinderTeam.palfinder.meetups.MeetUpRepository
+import com.github.palFinderTeam.palfinder.profile.ProfileService
+import com.github.palFinderTeam.palfinder.utils.time.TimeService
+import dagger.hilt.android.testing.HiltAndroidRule
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import javax.inject.Inject
 
 class NotificationTest {
     private val uiDevice by lazy { UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()) }
@@ -22,6 +32,31 @@ class NotificationTest {
     @Rule
     @JvmField
     val activityRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
+
+    private lateinit var notificationService: NotificationService
+
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var meetUpRepository: MeetUpRepository
+
+    @Inject
+    lateinit var profileRepository: ProfileService
+
+    @Inject
+    lateinit var chatService: ChatService
+
+    @Inject
+    lateinit var timeService: TimeService
+
+    @Before
+    fun setup(){
+        hiltRule.inject()
+        notificationService = NotificationService(timeService, meetUpRepository, profileRepository, chatService)
+        val context: Context = ApplicationProvider.getApplicationContext()
+        DictionaryCache.clearAllTempCaches(context)
+    }
 
 
     @Test
@@ -60,5 +95,21 @@ class NotificationTest {
         assertEquals(expectedTitle, title.text)
         assertTrue(text.text.startsWith(expectedContent))
         uiDevice.findObject(By.textStartsWith("Clear all")).click()
+    }
+
+    @Test
+    fun scheduleID() {
+        val context: Context = ApplicationProvider.getApplicationContext()
+
+        val expectedTitle = "title"
+        val expectedContent = "content"
+
+        val handler = NotificationHandler(context)
+        handler.schedule(Calendar.getInstance(), R.string.testNotifTitle,R.string.testNotifContent, R.drawable.icon_beer)
+
+        val cache = DictionaryCache("notification", CachedNotification::class.java, false, context)
+        assertTrue(cache.getAll().any { it.content == expectedContent && it.title == expectedTitle })
+
+        notificationService.action()
     }
 }
