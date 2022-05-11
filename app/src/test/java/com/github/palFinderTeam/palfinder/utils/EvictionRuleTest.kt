1c24e1212693c9b48daf6ce3ec8a4b48fc185394
Christian Fattebert <christian.fattebert@epfl.ch>
2022-05-11 10:25:30 +0200

    fix test

--- app/src/test/java/com/github/palFinderTeam/palfinder/utils/EvictionRuleTest.kt
+++ app/src/test/java/com/github/palFinderTeam/palfinder/utils/EvictionRuleTest.kt
@@ -1,32 +1,13 @@
 package com.github.palFinderTeam.palfinder.utils
 
 import android.icu.util.Calendar
-import com.github.palFinderTeam.palfinder.utils.time.TimeService
-import dagger.hilt.android.testing.HiltAndroidRule
-import dagger.hilt.android.testing.HiltAndroidTest
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
-import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito
 import java.io.File
-import javax.inject.Inject
 
-@OptIn(ExperimentalCoroutinesApi::class)
-@HiltAndroidTest
 class EvictionRuleTest {
-    @get:Rule
-    val hiltRule = HiltAndroidRule(this)
-
-    @Inject
-    lateinit var timeService: TimeService
-
-    @Before
-    fun setup(){
-        hiltRule.inject()
-    }
 
     @Test
     fun test(){
@@ -34,7 +15,8 @@ class EvictionRuleTest {
         Mockito.`when`(file.lastModified()).thenReturn(0)
         val date1 = Mockito.mock(Calendar::class.java)
         Mockito.`when`(date1.timeInMillis).thenReturn(100000)
-        (timeService as UIMockTimeServiceModule.UIMockTimeService).setDate(date1)
+        val timeService = MockTimeService()
+        timeService.setDate(date1)
         assertThat(evictAfterXMinutes(1, timeService)(file), `is`(true))
     }
 }
