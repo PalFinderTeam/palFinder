37d38c739611b71298dbb2d94fc1fc024027f35c
pgilliar <patrick.gilliard@epfl.ch>
2022-05-12 16:19:58 +0200

    reordered enum

--- app/src/main/java/com/github/palFinderTeam/palfinder/utils/PrivacySettings.kt
+++ app/src/main/java/com/github/palFinderTeam/palfinder/utils/PrivacySettings.kt
@@ -1,8 +1,8 @@
 package com.github.palFinderTeam.palfinder.utils
 
 enum class PrivacySettings(val stringPrivacysettings: String) {
-    PUBLIC("public"),
     PRIVATE("private"),
+    PUBLIC("public"),
     FRIENDS("friends only");
     companion object{
         fun from(type: String?): PrivacySettings = values().find { it.stringPrivacysettings == type } ?: PRIVATE
