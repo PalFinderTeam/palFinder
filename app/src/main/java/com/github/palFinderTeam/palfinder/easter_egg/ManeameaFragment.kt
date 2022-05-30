42f2c9b07e4f89f6b92166a77b9dfb0cbcbbeb25
atappy <antoine.tappy@epfl.ch>
2022-05-30 14:37:44 +0200

    try to make tests pass

--- app/src/main/java/com/github/palFinderTeam/palfinder/easter_egg/ManeameaFragment.kt
+++ app/src/main/java/com/github/palFinderTeam/palfinder/easter_egg/ManeameaFragment.kt
@@ -26,8 +26,6 @@ class ManeameaFragment : DialogFragment(){
         val videoView = v.findViewById<VideoView>(R.id.easter_egg_video)
         val uri = Uri.parse("https://i.imgur.com/Daoab5M.mp4")
         videoView.setVideoURI(uri)
-        //videoView.scaleX = 2.0F
-        //videoView.scaleY = 2.0F
         videoView.setOnPreparedListener { videoView.start() }
         videoView.setOnCompletionListener {
             dialog?.dismiss()
